a9432aa03ce5a340c0a017db4596d763096b4fcd
waterdelay <397249058@qq.com>
2018-12-29 08:48:25 +0800

    重新提交

---
+++ webpack-demo/main.js
@@ -0,0 +1,15 @@
+// 这是 main.js 是我们项目的JS入口文件
+
+// 1. 导入 Jquery
+// import *** from *** 是ES6中导入模块的方式
+// 由于 ES6的代码，太高级了，浏览器解析不了，所以，这一行执行会报错
+import $ from 'jquery'
+
+import "./src/css/index.css"
+
+import "./src/css/1.less"
+
+$(function(){
+    $("li:odd").css("backgroundColor","skyblue")
+    $("li:even").css("backgroundColor","orange")
+})
